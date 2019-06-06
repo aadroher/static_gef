1ce5b732a8a042d2ab3479d22be55a7638ee1293
Armand Adroher <aadroher@gmail.com>
2019-06-07 00:09:55 +0100

    Adds basic scraping of activities.

--- src/scrap.js
+++ src/scrap.js
@@ -3,28 +3,12 @@ import cheerio from 'cheerio';
 
 const baseUrl = 'https://www.grupdestudisfenomenologics.org';
 
-const activitiesPage = {
+const activitiesIndexPageDetails = {
   path: '/ca/node/50',
   pages: 4,
 };
 
-const parseActivitiesPage = pageBody => {
-  const $ = cheerio.load(pageBody);
-  return {
-    title: $('#page-title')
-      .text()
-      .trim(),
-    introText: $('.field-type-text-with-summary')
-      .first()
-      .text()
-      .trim(),
-    activities: $('.view-actualitat h2 a')
-      .toArray()
-      .map(anchor => `${baseUrl}${$(anchor).attr('href')}`),
-  };
-};
-
-const getPageBody = async url =>
+const getPageRawText = async url =>
   fetch(url).then(response => {
     const msg = `Fetched: ${url}`;
     console.info(msg);
@@ -37,16 +21,65 @@ const getPageBody = async url =>
     }
   });
 
+const parseActivityPage = pageData => {
+  const { url, pageRawText } = pageData;
+  const $ = cheerio.load(pageRawText);
+  const title = $('#page-title')
+    .text()
+    .trim();
+  const createdAt = $('[property="dc:date dc:created"]').attr('content');
+  const body = $('.field-name-body')
+    .text()
+    .trim();
+  return {
+    url,
+    title,
+    createdAt,
+    body,
+  };
+};
+
+const parseActivitiesIndexPage = async pageBody => {
+  const $ = cheerio.load(pageBody);
+  const activityPageBodies = await Promise.all(
+    $('.view-actualitat h2 a')
+      .toArray()
+      .map(anchor => `${baseUrl}${$(anchor).attr('href')}`)
+      .map(url =>
+        getPageRawText(url).then(pageRawText => ({
+          pageRawText,
+          url,
+        }))
+      )
+  );
+
+  const activities = activityPageBodies.map(parseActivityPage);
+
+  return {
+    title: $('#page-title')
+      .text()
+      .trim(),
+    introText: $('.field-type-text-with-summary')
+      .first()
+      .text()
+      .trim(),
+    activities,
+  };
+};
+
 const getActivities = async () => {
   const activitiesPagesBodies = await Promise.all(
-    [...new Array(activitiesPage.pages).keys()].map(i => {
-      const { path } = activitiesPage;
+    [...new Array(activitiesIndexPageDetails.pages).keys()].map(i => {
+      const { path } = activitiesIndexPageDetails;
       const url = `${baseUrl}${path}?page=${i}`;
-      return getPageBody(url);
+      return getPageRawText(url);
     })
   );
   // console.log(activitiesPagesBodies);
-  return activitiesPagesBodies.map(parseActivitiesPage).reduce(
+  const activityIndexPages = await Promise.all(
+    activitiesPagesBodies.map(parseActivitiesIndexPage)
+  );
+  return activityIndexPages.reduce(
     (
       { activities: previousActivities = [] },
       { title, introText, activities }
