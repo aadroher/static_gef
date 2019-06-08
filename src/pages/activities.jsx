358199c5a99d9d0ebb27d9efd728f5c67b11fb7b
Armand Adroher <aadroher@gmail.com>
2019-06-08 03:36:13 +0100

    First steps adding front end.

--- src/pages/activities.jsx
+++ src/pages/activities.jsx
@@ -4,15 +4,20 @@ import { graphql } from 'gatsby';
 const Activities = ({ data }) => (
   <div className="page-container">
     <h1>Activitats</h1>
-    <p>{data.site.siteMetadata.description}</p>
+    <p>{JSON.stringify(data, null, 2)}</p>
   </div>
 );
 
 export const query = graphql`
   query ActivitiesQuery {
-    site {
-      siteMetadata {
-        description
+    allFile(filter: { extension: { eq: "md" } }) {
+      edges {
+        node {
+          id
+          childMarkdownRemark {
+            html
+          }
+        }
       }
     }
   }
