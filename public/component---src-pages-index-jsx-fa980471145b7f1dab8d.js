35ddfebae71584557bfacb2965100d441fd86b83
Armand Adroher <aadroher@gmail.com>
2019-06-08 01:22:46 +0100

    Adds public folder.

---
+++ public/component---src-pages-index-jsx-fa980471145b7f1dab8d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{141:function(n,e,o){"use strict";o.r(e);var t=o(0),l=o.n(t);e.default=function(){return l.a.createElement("h1",null,"Hello World!")}}}]);
+//# sourceMappingURL=component---src-pages-index-jsx-fa980471145b7f1dab8d.js.map
