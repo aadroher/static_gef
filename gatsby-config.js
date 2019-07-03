febe848903ac7598e60674467b8107adac0406e4
Armand Adroher <aadroher@gmail.com>
2019-07-03 22:55:15 +0100

    Adds Netlify plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,5 +35,11 @@ module.exports = {
         minify: false,
       },
     },
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
   ],
 };
