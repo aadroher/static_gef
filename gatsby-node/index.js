14b98cf9e0fd60d76239759f752c2d8b4bb33a4a
Armand Adroher <aadroher@gmail.com>
2020-10-17 18:30:08 +0200

    Creates new type.

--- gatsby-node/index.js
+++ gatsby-node/index.js
@@ -1,11 +1,24 @@
 const path = require('path');
 const { createFilePath } = require('gatsby-source-filesystem');
+var { graphql, buildSchema } = require('graphql');
+const activityType = require('./types/activity.graphql');
+
+const schema = /* GraphQL */ `
+  type Activity implements Node {
+    name: String
+  }
+`;
+
+const createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions;
+  createTypes(schema);
+};
 
 const onCreateNode = ({ node, getNode, actions }) => {
   const {
     internal: { type }
   } = node;
-  const { createNodeField } = actions;
+  const { createNodeField, createNode } = actions;
   if (type === 'MarkdownRemark') {
     const slug = createFilePath({ node, getNode, basePath: 'pages' });
     createNodeField({
@@ -53,6 +66,7 @@ const createPages = async ({ graphql, actions }) => {
 };
 
 module.exports = {
+  createSchemaCustomization,
   onCreateNode,
   createPages
 };
