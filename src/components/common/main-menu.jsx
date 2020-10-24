fc18dbe69c7bb97a34980f859fbc029b78c80421
Armand Adroher <aadroher@gmail.com>
2020-10-24 22:34:48 +0200

    Hardcodes menu data.

--- src/components/common/main-menu.jsx
+++ src/components/common/main-menu.jsx
@@ -1,40 +1,9 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
-import { graphql, useStaticQuery } from 'gatsby';
 
 import MainMenuLink from './main-menu-link';
 import GlobalContext from './global-context';
-
-const staticQuery = graphql`
-  query MainMenu {
-    indexPages: allSitePage(filter: { context: { isIndex: { eq: true } } }) {
-      nodes {
-        path
-        context {
-          markdownNodeId
-          isIndex
-          languageCode
-          collectionmarkdownNodeIds
-        }
-      }
-    }
-    markdownContent: allMarkdownRemark {
-      nodes {
-        id
-        frontmatter {
-          contentType
-          title
-          languageCode
-        }
-        parent {
-          ... on File {
-            relativePath
-          }
-        }
-      }
-    }
-  }
-`;
+import mainMenuData from '../../../data/menus/main.yml';
 
 const MenuList = styled.ul`
   margin: 0;
@@ -65,37 +34,24 @@ const getLocale = path => {
   return localeUrlPathComponent;
 };
 
-const getMenuItemData = ({ locale, indexPages, markdownContent }) =>
-  indexPages.nodes
-    .filter(({ path }) => locale === getLocale(path))
-    .map(({ path, context: { markdownNodeId } }) => {
-      const {
-        frontmatter: { title: label }
-      } = markdownContent.nodes.find(({ id }) => id === markdownNodeId);
-
-      return {
-        path,
-        label
-      };
-    });
+const getMenuItemData = ({ locale, mainMenuData }) => mainMenuData[locale];
 
 const MainMenu = () => {
   const {
     location: { pathname: currentPath }
   } = useContext(GlobalContext);
-  const staticQueryData = useStaticQuery(staticQuery);
   const locale = getLocale(currentPath);
-  const menuItemData = getMenuItemData({ locale, ...staticQueryData });
+  const menuItemData = getMenuItemData({ locale, mainMenuData });
 
   return (
     <nav>
       <MenuList>
-        {menuItemData.map(({ path, label }) => (
+        {menuItemData.map(({ path, name }) => (
           <li key={path}>
             <MainMenuLink
               path={path}
               enabled={getEnabled({ currentPath, path })}
-              label={label}
+              label={name}
             />
           </li>
         ))}
